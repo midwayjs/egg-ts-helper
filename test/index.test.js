60508b9896d08dc1d74a9b264afd66395711b8c2
Harry Chen <czy88840616@gmail.com>
2021-05-03 12:48:01 +0800

    test: update

--- test/index.test.js
+++ test/index.test.js
@@ -7,17 +7,17 @@ describe('test/index.test.js', () => {
     process.chdir(join(__dirname, 'fixtures'));
     require(join(__dirname, '../index'));
     // extend
-    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/application.d.ts')));
-    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/context.d.ts')));
-    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/helper.d.ts')));
-    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/request.d.ts')));
-    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/request.unittest.d.ts')));
-    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/response.d.ts')));
+    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/application.d.ts'))).toBeTruthy();
+    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/context.d.ts'))).toBeTruthy();
+    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/helper.d.ts'))).toBeTruthy();
+    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/request.d.ts'))).toBeTruthy();
+    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/request.unittest.d.ts'))).toBeTruthy();
+    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/extend/response.d.ts'))).toBeTruthy();
 
     // app
-    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/index.d.ts')));
+    expect(fs.existsSync(join(__dirname, 'fixtures/typings/app/index.d.ts'))).toBeTruthy();
     //config
-    expect(fs.existsSync(join(__dirname, 'fixtures/typings/config/index.d.ts')));
-    expect(fs.existsSync(join(__dirname, 'fixtures/typings/config/plugin.d.ts')));
+    expect(fs.existsSync(join(__dirname, 'fixtures/typings/config/index.d.ts'))).toBeTruthy();
+    expect(fs.existsSync(join(__dirname, 'fixtures/typings/config/plugin.d.ts'))).toBeTruthy();
   });
 });
